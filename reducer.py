3c690ccfe6b1db0abb852ff5ff6b6dd746cff743
Charles Stover <charlesstover@charlesstover.com>
2018-04-05 09:16:11 -0500

    more reducer to Py

--- reducer.py
+++ reducer.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from random import randint, seed
+seed(datetime.now())
+
 WINDOW_WIDTH = 640
 
 def galoombaCollisionX(self, obj):
@@ -41,82 +45,84 @@ def galoombaController(self):
       if self.props['frame'] > 15:
         self.frame = 0
 
-def reducer(type, action):
-  if type == 'ADD_GALOOMBA':
-    var speed = Math.random() * 4 - 2;
-    self.objects.push(new Sprite('images/galoomba.gif', {
-      collisionX: galoombaCollisionX,
-      collisionY: galoombaCollisionY,
-      controller: galoombaController,
-      dead: 0,
-      frame: 0,
-      height: 15,
-      horizontalAcceleration: speed < 0 ? -0.1 : 0.1,
-      horizontalVelocity: speed,
-      maxHorizontalVelocity: 2,
-      maxVerticalVelocity: -5,
-      verticalAcceleration: -1,
-      sprite:
-        speed > 0
-        ? [48, 0]
-        : [0, 0],
-      type: 'galoomba',
-      width: 16,
-      x: mario.x + Math.random() * document.body.clientWidth - document.body.clientWidth / 2,
-      y: document.body.clientHeight
-    }))
-  elif type == 'ADD_TUBE':
-    var TUBE_WIDTH = 32;
-    self.objects.push(new Sprite(null, {
-      className: 'tube',
-      height: document.body.clientHeight - action.y,
-      sheet: ['images/tube.gif', 'images/tube.gif'],
-      sprite: [[0, 0], [TUBE_WIDTH, 0]],
-      type: 'tube',
-      x: mario.x + action.x - document.body.clientWidth / 2 - TUBE_WIDTH / 2,
-      y: 0,
-      width: TUBE_WIDTH
-    }))
-  elif type == 'BRAKE_LEFT':
-    if (mario.walking == -1) {
-      mario.set('horizontalAcceleration', 0);
-      mario.set('horizontalVelocity', 0);
-      mario.set('walking', 0);
-    }
-  elif type =='BRAKE_RIGHT':
-    if (mario.walking == 1) {
-      mario.set('horizontalAcceleration', 0);
-      mario.set('horizontalVelocity', 0);
-      mario.set('walking', 0);
-    }
-  elif type == 'DELETE':
-    for (var x of Object.keys(self.objects)) {
-      if (self.objects[x] == action) {
-        document.body.removeChild(self.objects[x].element);
-        delete self.objects[x];
-        break;
+def reducer(model):
+  def r(type, action):
+    if type == 'ADD_GALOOMBA':
+      speed = randint(-2, 2)
+      if speed < 0:
+        horizontalAcceleration = -0.1
+      else
+        horizontalAcceleration = 0.1
+      model.objects.push(Sprite('images/galoomba.gif', {
+        collisionX: galoombaCollisionX,
+        collisionY: galoombaCollisionY,
+        controller: galoombaController,
+        dead: 0,
+        frame: 0,
+        height: 15,
+        horizontalAcceleration: horizontalAcceleration,
+        horizontalVelocity: speed,
+        maxHorizontalVelocity: 2,
+        maxVerticalVelocity: -5,
+        verticalAcceleration: -1,
+        sprite:
+          speed > 0
+          ? [48, 0]
+          : [0, 0],
+        type: 'galoomba',
+        width: 16,
+        x: mario.x + Math.random() * document.body.clientWidth - document.body.clientWidth / 2,
+        y: document.body.clientHeight
+      }))
+    elif type == 'ADD_TUBE':
+      TUBE_WIDTH = 32
+      model.objects.push(new Sprite(null, {
+        className: 'tube',
+        height: document.body.clientHeight - action.y,
+        sheet: ['images/tube.gif', 'images/tube.gif'],
+        sprite: [[0, 0], [TUBE_WIDTH, 0]],
+        type: 'tube',
+        x: mario.x + action.x - document.body.clientWidth / 2 - TUBE_WIDTH / 2,
+        y: 0,
+        width: TUBE_WIDTH
+      }))
+    elif type == 'BRAKE_LEFT':
+      if mario.props['walking'] == -1:
+        mario.set('horizontalAcceleration', 0)
+        mario.set('horizontalVelocity', 0)
+        mario.set('walking', 0)
+    elif type =='BRAKE_RIGHT':
+      if mario.props['walking'] == 1:
+        mario.set('horizontalAcceleration', 0)
+        mario.set('horizontalVelocity', 0)
+        mario.set('walking', 0)
+    elif type == 'DELETE':
+      for (var x of Object.keys(model.objects)) {
+        if model.objects[x] == action:
+          document.body.removeChild(model.objects[x].element)
+          delete model.objects[x]
+          break;
+        }
       }
-    }
-  elif type == 'JUMP':
-    if (!mario.falling) {
-      mario.set('verticalVelocity', mario.jumpVelocity);
-    }
-  elif type == 'RESIZE_WINDOW':
-    // document.body.style.setProperty('padding-left', Math.round(document.body.clientWidth / 2) + 'px');
-    document.body.style.setProperty('padding-top', document.body.clientHeight + 'px');
-  elif type == 'SHOOT_FIREBALL':
-    self.objects.push(Fireball());
-  elif type == 'WALK_LEFT':
-    if (mario.walking !== -1) {
-      mario.set('direction', false);
-      mario.set('horizontalAcceleration', -1 * mario.walkAcceleration);
-      mario.set('horizontalVelocity', -0.25 * mario.horizontalVelocity);
-      mario.set('walking', -1);
-    }
-  elif type == 'WALK_RIGHT':
-    if (mario.walking !== 1) {
-      mario.set('direction', true);
-      mario.set('horizontalAcceleration', mario.walkAcceleration);
-      mario.set('horizontalVelocity', -0.25 * mario.horizontalVelocity);
-      mario.set('walking', 1);
-    }
+    elif type == 'JUMP':
+      if not mario.props['falling']:
+        mario.set('verticalVelocity', mario.jumpVelocity)
+    elif type == 'RESIZE_WINDOW':
+      # document.body.style.setProperty('padding-left', Math.round(document.body.clientWidth / 2) + 'px');
+      document.body.style.setProperty('padding-top', document.body.clientHeight + 'px')
+    elif type == 'SHOOT_FIREBALL':
+      model.objects.push(Fireball())
+    elif type == 'WALK_LEFT':
+      if mario.props['walking'] !== -1:
+        mario.set('direction', False)
+        mario.set('horizontalAcceleration', -1 * mario.props['walkAcceleration'])
+        mario.set('horizontalVelocity', -0.25 * mario.props['horizontalVelocity'])
+        mario.set('walking', -1)
+    elif type == 'WALK_RIGHT':
+      if mario.props['walking'] !== 1:
+        mario.set('direction', True)
+        mario.set('horizontalAcceleration', mario.props['walkAcceleration'])
+        mario.set('horizontalVelocity', -0.25 * mario.props['horizontalVelocity'])
+        mario.set('walking', 1)
+      }
+  return r
