8a0b6a64ea1ae5374a1bf4c98094664aa81b5d96
Charles Stover <charlesstover@charlesstover.com>
2018-04-05 08:02:45 -0500

    Converted Mario methods to use props dict

--- scripts/mario.py
+++ scripts/mario.py
@@ -1,74 +1,74 @@
 from sprite import Sprite
 
 def collisionX(self, obj):
-  if obj.type == 'galoomba':
+  if obj.props['type'] == 'galoomba':
     self.set('horizontalVelocity', 0)
 
     # Collision while running right.
-    if self.direction:
-      self.set('x', obj.x - self.width - 0.1)
+    if self.props['direction']:
+      self.set('x', obj.props['x'] - self.props['width'] - 0.1)
 
     # Collision while running left.
     else:
-      self.set('x', obj.x + obj.width + 0.1)
+      self.set('x', obj.props['x'] + obj.props['width'] + 0.1)
 
-  elif obj.type == 'tube':
+  elif obj.props['type'] == 'tube':
     self.set('horizontalVelocity', 0)
 
     # Collision while running right.
-    if self.direction:
-      self.set('x', obj.x - self.width - 0.1)
+    if self.props['direction']:
+      self.set('x', obj.props['x'] - self.props['width'] - 0.1)
 
     # Collision while running left.
     else:
-      self.set('x', obj.x + obj.width + 0.1)
+      self.set('x', obj.props['x'] + obj.props['width'] + 0.1)
 
 def collisionY(self, obj):
   if obj.type == 'galoomba':
-    self.set('verticalVelocity', -1 * self.verticalVelocity)
+    self.set('verticalVelocity', -1 * self.props['verticalVelocity'])
 
   elif obj.type == 'tube':
     self.set('falling', False)
     self.set('verticalVelocity', 0)
-    self.set('y', obj.y + obj.height + 0.1)
+    self.set('y', obj.props['y'] + obj.props['height'] + 0.1)
 
 def controller(self):
   xPerFrame = 7
-  if self.walking:
-    self.walkFrame = (self.walkFrame + 1) % (xPerFrame * 4)
+  if self.props['walking']:
+    self.props['walkFrame'] = (self.props['walkFrame'] + 1) % (xPerFrame * 4)
   else:
-    self.walkFrame = 0
+    self.props['walkFrame'] = 0
 
   # Set sprite.
-  if self.direction:
+  if self.props['direction']:
     def adjustWidth(self, width):
       previousWidth = self.width
       self.set('width', width)
       self.set('x', self.x + previousWidth - width)
-    if self.walking:
-      if self.walkFrame < xPerFrame:
-        if self.horizontalVelocity == self.maxHorizontalVelocity:
+    if self.props['walking']:
+      if self.props['walkFrame'] < xPerFrame:
+        if self.props['horizontalVelocity'] == self.props['maxHorizontalVelocity']:
           self.set('sprite', [95, 0])
           adjustWidth(self, 18)
         else:
           self.set('sprite', [0, 0])
           adjustWidth(self, 15)
-      elif self.walkFrame < xPerFrame * 2:
-        if self.horizontalVelocity == self.maxHorizontalVelocity:
+      elif self.props['walkFrame'] < xPerFrame * 2:
+        if self.props['horizontalVelocity'] == self.props['maxHorizontalVelocity']:
           self.set('sprite', [113, 0])
           adjustWidth(self, 18)
         else:
           self.set('sprite', [15, 0])
           adjustWidth(self, 15)
-      elif self.walkFrame < xPerFrame * 3:
-        if self.horizontalVelocity == self.maxHorizontalVelocity:
+      elif self.props['walkFrame'] < xPerFrame * 3:
+        if self.props['horizontalVelocity'] == self.props['maxHorizontalVelocity']:
           self.set('sprite', [95, 0])
           adjustWidth(self, 18)
         else:
           self.set('sprite', [0, 0])
           adjustWidth(self, 15)
       else:
-        if self.horizontalVelocity == self.maxHorizontalVelocity:
+        if self.props['horizontalVelocity'] == self.props['maxHorizontalVelocity']:
           self.set('sprite', [131, 0])
           adjustWidth(self, 18)
         else:
@@ -78,30 +78,30 @@ def controller(self):
       self.set('sprite', [0, 0])
       adjustWidth(self, 15)
   else:
-    if self.walking:
-      if self.walkFrame < xPerFrame:
-        if Math.abs(self.horizontalVelocity) == self.maxHorizontalVelocity:
+    if self.props['walking']:
+      if self.props['walkFrame'] < xPerFrame:
+        if abs(self.props['horizontalVelocity']) == self.props['maxHorizontalVelocity']:
           self.set('sprite', [95, 28])
           self.set('width', 18)
         else:
           self.set('sprite', [80, 28])
           self.set('width', 15)
-      elif self.walkFrame < xPerFrame * 2:
-        if Math.abs(self.horizontalVelocity) == self.maxHorizontalVelocity:
+      elif self.props['walkFrame'] < xPerFrame * 2:
+        if abs(self.props['horizontalVelocity']) == self.props['maxHorizontalVelocity']:
           self.set('sprite', [131, 28])
           self.set('width', 18)
         else:
           self.set('sprite', [65, 28])
           self.set('width', 15)
-      elif self.walkFrame < xPerFrame * 3:
-        if Math.abs(self.horizontalVelocity) == self.maxHorizontalVelocity:
+      elif self.props['walkFrame'] < xPerFrame * 3:
+        if abs(self.props['horizontalVelocity']) == self.props['maxHorizontalVelocity']:
           self.set('sprite', [95, 28])
           self.set('width', 18)
         else:
           self.set('sprite', [80, 28])
           self.set('width', 15)
       else:
-        if Math.abs(self.horizontalVelocity) == self.maxHorizontalVelocity:
+        if abs(self.props['horizontalVelocity']) == self.props['maxHorizontalVelocity']:
           self.set('sprite', [113, 28])
           self.set('width', 18)
         else:
@@ -112,8 +112,8 @@ def controller(self):
       self.set('width', 15)
 
   # Set background.
-  document.body.style.setProperty('background-position', (Math.round(window.model.renders / 10) % 4) * -512 + (-1 * self.x) / 2 + 'px center')
-  document.body.style.setProperty('margin-left', (-1 * self.x + document.body.clientWidth / 2) + 'px')
+  # document.body.style.setProperty('background-position', (Math.round(window.model.renders / 10) % 4) * -512 + (-1 * self.x) / 2 + 'px center')
+  # document.body.style.setProperty('margin-left', (-1 * self.x + document.body.clientWidth / 2) + 'px')
 
 mario = Sprite('images/mario.gif', {
   'collisionX': collisionX,
